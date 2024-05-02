f121f1afbbccddfa432220315c721bbd2148fe1a
sahan-manusha <sahan.manusha123@gmail.com>
2024-05-02 12:13:14 +0530

    Add Search Bar To The Home Page

--- frontend/ui/src/App.jsx
+++ frontend/ui/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
-import SupplierList from './components/SupplierList'
+import SearchBar from './components/SearchBar'
 
 
 
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
     <Navbar/>
-    <SupplierList/>
+    <SearchBar/>
     </>
   )
 }
