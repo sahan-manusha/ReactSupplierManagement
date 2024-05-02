965b2ab436f13816e2cccc91b5d9234b8a43a53e
sahan-manusha <sahan.manusha123@gmail.com>
2024-05-02 12:06:43 +0530

    Edit Navbar

--- frontend/ui/src/components/Navbar.jsx
+++ frontend/ui/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><strong>Supplier Management</strong></a>
+        <a class="navbar-brand" href="#"><strong>Suppliers Management</strong></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
